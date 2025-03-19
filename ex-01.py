d68ff7fe76c78f8422dc088f0353627d638a87e1
021gustavoo <chefaoff56@gmail.com>
2025-03-18 21:55:21 -0300

    Create ex-01.py

---
+++ ex-01.py
@@ -0,0 +1,24 @@
+resposta do ex 8
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print("Seu nome é:", nome)
+print("Sua idade é:", idade)
+
+resposta do ex 9 
+numero1 = input("Digite o primeiro número: ")
+numero2 = input("Digite o segundo número: ")
+numero1 = int(numero1)
+numero2 = int(numero2)
+soma = numero1 + numero2
+print("A soma dos dois números é:", soma)
+
+resposta do ex 10
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) /4
+
+print("A média aritmética do aluno é:", media)
